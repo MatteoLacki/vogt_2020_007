0f2a27f2a3b192d604e6756083f830361749023d
matteo <matteo.lacki@gmail.com>
2020-09-02 16:25:37 +0200

    ordered the code: understand what can go wrong in R: t.test might neglect NAs

--- R/analyses.R
+++ R/analyses.R
@@ -16,10 +16,11 @@ Rw = dcast(Rl, accession+repl~Group, value.var = 'I')
 TenzerTest = function(A, B, cutoff=.5, bogus_pval=1, highly_significant_pvalue=0, ...){
   no_A = sum(is.na(A))/length(A) >= cutoff
   no_B = sum(is.na(B))/length(B) >= cutoff
-  if(!no_A & !no_B) return( t.test(A,B,...)$p.value )
+  if(!no_A & !no_B) return( t.test(A,B,...)$p.value ) # the test neglects NAs: not good!
   if(no_A & no_B) return(bogus_pval)
   return(highly_significant_pvalue)
 }
+
 ratio = function(A, B, fill_in_value=1){
   no_A = sum(is.na(A))/length(A) >= .5
   no_B = sum(is.na(B))/length(B) >= .5
@@ -30,27 +31,55 @@ ratio = function(A, B, fill_in_value=1){
   med_A / med_B
 }
 
-get_pvals = function(comp, Rw){
+p_adjust = function(p, ...){
+  r = p
+  correction_needed = p > 0 & p < 1 & !is.na(p)
+  r[correction_needed] = p.adjust(r[correction_needed], ...)
+  return(r)
+}
+
+get_pvals = function(comp, Rw, test, ratio){
   comp = c('accession',comp)
   Rw_comp = Rw[,..comp]
   colnames(Rw_comp) = c('accession','A','B')
-  res = Rw_comp[,.(pval = TenzerTest(A,B),
+  res = Rw_comp[,.(pval = test(A,B),
                    AoverB = ratio(A,B)),
                 by=.(accession)]
-  res[,`:=`(pval_bon= p.adjust(pval, method = 'bonferroni'),
-            pval_BH = p.adjust(pval, method = 'BH'),
-            pval_BY = p.adjust(pval, method = 'BY'))]
+  res[,`:=`(pval_bon= p_adjust(pval, method = 'bonferroni'),
+            pval_BH = p_adjust(pval, method = 'BH'),
+            pval_BY = p_adjust(pval, method = 'BY'))]
   res
 }
 
-r = rbindlist(lapply(comparisons, get_pvals, Rw=Rw), idcol='comp')
-save(r, file='data/results.Rd')
-rw = dcast(r, accession~comp, value.var = c('pval_bon','pval_BY', 'pval_BH', 'AoverB'), sep=": ")
-rw = rw[order(accession)]
-write.csv(rw, file='data/comparison.csv')
+r1 = rbindlist(lapply(comparisons, get_pvals, Rw, TenzerTest, ratio), idcol='comp')
+save(r1, file='data/results1.Rd')
+
+longify = function(r) dcast(r, accession~comp, value.var = c('pval_bon','pval_BY', 'pval_BH', 'AoverB'), sep=": ")[order(accession)]
+rw1 = longify(r1)
+write.csv(rw1, file='data/comparison1.csv')
+
+
+# another version
+
+TenzerTestSafer = function(A, B, cutoff=.5, bogus_pval=1, highly_significant_pvalue=0, ...){
+  A_NA_cnt = sum(is.na(A))
+  B_NA_cnt = sum(is.na(B))
+  no_A = A_NA_cnt / length(A) >= cutoff
+  no_B = B_NA_cnt / length(B) >= cutoff
+
+  if(length(A)-A_NA_cnt < 2 & length(B)-B_NA_cnt < 2) return(bogus_pval)
+  if(A_NA_cnt == length(A) & B_NA_cnt == length(B)) return( t.test(A,B,...)$p.value )
+  if(no_A & B_NA_cnt == length(B)) return(highly_significant_pvalue)
+  if(no_B & A_NA_cnt == length(A)) return(highly_significant_pvalue)
+  return(bogus_pval)
+}
+
+r2 = rbindlist(lapply(comparisons, get_pvals, Rw, TenzerTestSafer, ratio), idcol='comp')
+save(r2, file='data/results2.Rd')
+rw2 = longify(r2)
+write.csv(rw2, file='data/comparison1.csv')
 
 # Alternative procedure
-
 
 fill_values = function(X, X_small_I, X_fill_in_value){
   X_NA_cnt = sum(is.na(X))
@@ -82,34 +111,32 @@ RwFix = fill_low_values()
 TenzerTest2 = function(A, B, ...){
   if(any(is.na(A))) return(-1) # some NAs persist: bogus
   if(any(is.na(B))) return(-2) # some NAs persist: bogus
-  if(all(A == A[1]) & all(B == B[1])) return(1) # All outcomes are the same: t-test will fail.
-  if(length(A) < 2 | length(B) < 2) return(-3) # impossible to estimate st-dev: t-test will fail
-  
+  if(all(A == A[1]) & all(B == B[1])) return(-3) # All outcomes are the same: t-test will fail.
+  if(length(A) < 2) return(-4) # impossible to estimate st-dev in A: t-test will fail
+  if(length(B) < 2) return(-5) # impossible to estimate st-dev in B: t-test will fail
   return( t.test(A,B,...)$p.value )
 }
-
 ratio2 = function(A, B) median(A, na.rm=T) / median(B, na.rm=T)
 
-get_pvals2 = function(comp, RwFix){
-  comp = c('accession',comp)
-  Rw_comp = RwFix[,..comp]
-  colnames(Rw_comp) = c('accession','A','B')
-  res = Rw_comp[,.(pval = TenzerTest2(A,B),
-                   AoverB = ratio2(A,B)),
-                by=.(accession)]
-  res[,`:=`(pval_bon= p.adjust(pval, method = 'bonferroni'),
-            pval_BH = p.adjust(pval, method = 'BH'),
-            pval_BY = p.adjust(pval, method = 'BY'))]
-  res
-}
-
-r2 = rbindlist(lapply(comparisons, get_pvals2, RwFix), idcol='comp')
-save(r2, file='data/results2.Rd')
-rw2 = dcast(r2, accession~comp, value.var = c('pval_bon','pval_BY', 'pval_BH', 'AoverB'), sep=": ")
-rw2 = rw[order(accession)]
-write.csv(rw2, file='data/comparison2.csv')
+r3 = rbindlist(lapply(comparisons, get_pvals, RwFix, TenzerTest2, ratio2), idcol='comp')
+save(r3, file='data/results3.Rd')
+rw3 = longify(r3)
+write.csv(rw3, file='data/comparison3.csv')
 
 
+# further comparisons of the two approaches
+r1$method = 'old'
+r2$method = 'old_safer'
+r3$method = 'new'
+rr = rbind(r1, r2, r3)
+rr = dcast(rr, comp+accession~method, value.var = c('pval', 'pval_BY', 'pval_bon', 'AoverB'))
+
+with(rr, plot(pval_old, pval_new))
+with(rr[pval_new>=0], plot(pval_old, pval_new))
+with(rr, plot(AoverB_old, AoverB_new, asp=1))
+
+NAs = Rl[,.(NAcnt=sum(is.na(I)), OBScnt=length(I)),by=.(Group,accession)]
+plot(NAs[, .N, by=NAcnt][order(NAcnt)], type='b', ylim=c(0,5000)) # relatively few observations 
 
 
 
--- R/plot.R
+++ R/plot.R
@@ -3,20 +3,32 @@ library(data.table)
 library(patchwork)
 library(ggeasy)
 
-load('data/results.Rd')
+load('data/results1.Rd')
 load('data/results2.Rd')
+load('data/results3.Rd')
 
-erupt_volcano = function(r, thr=c(.001,.01,.05,.1)){
-  volcano = ggplot(r[!is.na(AoverB) & pval_BH > 0]) +
+erupt_volcano = function(r) ggplot(r) +
     geom_point(aes(x=AoverB, y=-log10(pval_BH)), size=.4) +
     scale_x_log10()+
     facet_grid(.~comp)+
     xlab('A / B') +
     ylab('-log10(pvalue[Benjamini & Hochberg])') 
-  
+
+thr_lines = function(thr=c(.001,.01,.05,.1)){
   lines = data.table(thr=thr)
   lines[,threshold := ordered(thr)]
-  volcano + geom_hline(data=lines, aes(yintercept=-log10(thr), color=threshold))
+  geom_hline(data=lines, aes(yintercept=-log10(thr), color=threshold))
 }
 
-(erupt_volcano(r) + ggtitle('Old Method')) / (erupt_volcano(r2) + ggtitle('New Method'))
+r_good = r[!is.na(AoverB) & pval_BH > 0 & pval_BH < 1 & !is.na(pval_BH)]
+r_good$method = 'old'
+r2_good = r2[!is.na(AoverB) & pval_BH > 0 & pval_BH < 1 & !is.na(pval_BH)]
+r2_good$method = 'new'
+
+r1$method='old'
+r2$method='old_safer'
+r3$method='new'
+
+erupt_volcano(rbind(r1, r3)) + facet_grid(method~comp) + thr_lines()
+
+
