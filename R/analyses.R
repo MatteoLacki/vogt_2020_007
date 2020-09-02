38e2f20d7684025ca77577b12809d6399ebdc0ea
matteo <matteo.lacki@gmail.com>
2020-09-02 13:37:56 +0200

    ordered code. added new method of imputation

--- R/analyses.R
+++ R/analyses.R
@@ -20,41 +20,6 @@ TenzerTest = function(A, B, cutoff=.5, bogus_pval=1, highly_significant_pvalue=0
   if(no_A & no_B) return(bogus_pval)
   return(highly_significant_pvalue)
 }
-
-Rl[,.(p=c(0,.001), q=quantile(I, probs=c(0,.001), na.rm=T)), by=Group]
-
-fill_values = function(X, X_small_I, X_fill_in_value){
-  X_NA_cnt = sum(is.na(X))
-  if(X_NA_cnt == length(X)) return(replicate(length(X), X_fill_in_value))
-  if(X[!is.na(X)] <= X_small_I){
-    X[is.na(X)] = X_fill_in_value
-  }
-  return(X)
-}
- 
-TenzerTest2 = function(A, B, A_small_I, B_small_I, A_fill_in_value, B_fill_in_value, ...){
-  A = fill_values(A, A_small_I, A_fill_in_value)
-  B = fill_values(B, B_small_I, B_fill_in_value)
-  
-  if(any(is.na(A))) return(NA) # some NAs persist: bogus
-  if(any(is.na(B))) return(NA) # some NAs persist: bogus
-  if(all(A == A[1]) & all(B == B[1])) return(1) # All outcomes are the same: t-test will fail.
-  if(length(A) < 2) return(NA) # impossible to estimate st-dev for A: t-test will fail
-  if(length(B) < 2) return(NA) # impossible to estimate st-dev for A: t-test will fail
-  
-  return( t.test(A,B,...)$p.value )
-}
-
-ratio2 = function(A, B, A_small_I, B_small_I, A_fill_in_value, B_fill_in_value){
-  A = fill_values(A, A_small_I, A_fill_in_value)
-  B = fill_values(B, B_small_I, B_fill_in_value)
-  med_A = median(A, na.rm=T)
-  med_B = median(B, na.rm=T)
-  med_A / med_B
-}
-
-
-
 ratio = function(A, B, fill_in_value=1){
   no_A = sum(is.na(A))/length(A) >= .5
   no_B = sum(is.na(B))/length(B) >= .5
@@ -64,7 +29,6 @@ ratio = function(A, B, fill_in_value=1){
   med_B = median(B, na.rm=T)
   med_A / med_B
 }
-
 
 get_pvals = function(comp, Rw){
   comp = c('accession',comp)
@@ -80,8 +44,72 @@ get_pvals = function(comp, Rw){
 }
 
 r = rbindlist(lapply(comparisons, get_pvals, Rw=Rw), idcol='comp')
-save(r, file='data/results.Rda')
+save(r, file='data/results.Rd')
 rw = dcast(r, accession~comp, value.var = c('pval_bon','pval_BY', 'pval_BH', 'AoverB'), sep=": ")
 rw = rw[order(accession)]
 write.csv(rw, file='data/comparison.csv')
+
+# Alternative procedure
+
+
+fill_values = function(X, X_small_I, X_fill_in_value){
+  X_NA_cnt = sum(is.na(X))
+  if(X_NA_cnt == length(X)) return(replicate(length(X), X_fill_in_value))
+  if(any(X[!is.na(X)] <= X_small_I)){
+    X[is.na(X)] = X_fill_in_value
+  }
+  return(X)
+}
+
+fill_low_values = function(){
+  RwFix = Rw[,.(accession, repl)]
+  conditions = unique(unlist(comparisons, use.names = F))
+  p = .001
+  for(cond in conditions){
+    cols = c('accession', cond)
+    x = Rw[,..cols]
+    colnames(x) = c('accession', 'X')
+    x_small_I = quantile(x$X, p, na.rm=T)
+    x_fill_in_value = min(x$X, na.rm=T)
+    x[,fX:=fill_values(X, x_small_I, x_fill_in_value),by=accession]
+    set(RwFix, j=cond, value=x$fX)  
+  }
+  return(RwFix)
+}
+
+RwFix = fill_low_values()
+
+TenzerTest2 = function(A, B, ...){
+  if(any(is.na(A))) return(-1) # some NAs persist: bogus
+  if(any(is.na(B))) return(-2) # some NAs persist: bogus
+  if(all(A == A[1]) & all(B == B[1])) return(1) # All outcomes are the same: t-test will fail.
+  if(length(A) < 2 | length(B) < 2) return(-3) # impossible to estimate st-dev: t-test will fail
+  
+  return( t.test(A,B,...)$p.value )
+}
+
+ratio2 = function(A, B) median(A, na.rm=T) / median(B, na.rm=T)
+
+get_pvals2 = function(comp, RwFix){
+  comp = c('accession',comp)
+  Rw_comp = RwFix[,..comp]
+  colnames(Rw_comp) = c('accession','A','B')
+  res = Rw_comp[,.(pval = TenzerTest2(A,B),
+                   AoverB = ratio2(A,B)),
+                by=.(accession)]
+  res[,`:=`(pval_bon= p.adjust(pval, method = 'bonferroni'),
+            pval_BH = p.adjust(pval, method = 'BH'),
+            pval_BY = p.adjust(pval, method = 'BY'))]
+  res
+}
+
+r2 = rbindlist(lapply(comparisons, get_pvals2, RwFix), idcol='comp')
+save(r2, file='data/results2.Rd')
+rw2 = dcast(r2, accession~comp, value.var = c('pval_bon','pval_BY', 'pval_BH', 'AoverB'), sep=": ")
+rw2 = rw[order(accession)]
+write.csv(rw2, file='data/comparison2.csv')
+
+
+
+
 
--- R/plot.R
+++ R/plot.R
@@ -1,19 +1,22 @@
 library(ggplot2)
 library(data.table)
+library(patchwork)
+library(ggeasy)
 
 load('data/results.Rd')
+load('data/results2.Rd')
 
-volcano = ggplot(r[!is.na(AoverB) & pval_BH > 0]) +
-  geom_point(aes(x=AoverB, y=-log10(pval_BH)), size=.4) +
-  scale_x_log10()+
-  facet_grid(.~comp)+
-  xlab('A / B') +
-  ylab('-log10(pvalue[Benjamini & Hochberg])')
-
-lines = data.table(thr=c(.001,.01,.5,.1))
-lines[,threshold := ordered(thr)]
-
-volcano + geom_hline(data=lines, aes(yintercept=-log10(thr), color=threshold))
+erupt_volcano = function(r, thr=c(.001,.01,.05,.1)){
+  volcano = ggplot(r[!is.na(AoverB) & pval_BH > 0]) +
+    geom_point(aes(x=AoverB, y=-log10(pval_BH)), size=.4) +
+    scale_x_log10()+
+    facet_grid(.~comp)+
+    xlab('A / B') +
+    ylab('-log10(pvalue[Benjamini & Hochberg])') 
   
+  lines = data.table(thr=thr)
+  lines[,threshold := ordered(thr)]
+  volcano + geom_hline(data=lines, aes(yintercept=-log10(thr), color=threshold))
+}
 
-
+(erupt_volcano(r) + ggtitle('Old Method')) / (erupt_volcano(r2) + ggtitle('New Method'))
